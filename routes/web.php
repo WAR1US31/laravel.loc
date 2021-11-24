4a07bf0f597541789ad833d8b739545a8b10ac4e
Dmitriy <dima@webdima31.ru>
2021-11-24 17:46:30 +0300

    Разобрались с роутами и создали несколько видов.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function() {
+   return 'Hello world';
+});*/
+
+Route::get('/', function () {
+    $res = 2 + 3;
+    $name = 'John';
+    return view('home', compact('res', 'name'));
+//    return view('home', ['res' => $res, 'name' => $name]);
+});
+
+/*Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::post('/send-email', function () {
+    if(!empty($_POST)) {
+        dump($_POST);
+    }
+    return 'Send Mail';
+});*/
+
+Route::match(['post', 'get'], '/contact', function () {
+    if(!empty($_POST)) {
+        dump($_POST);
+    }
+    return view('contact');
+})->name('contact');
+
+Route::view('/test', 'test');
