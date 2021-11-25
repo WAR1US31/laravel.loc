7ac4b663f5685b0f6f2a7fe0a9876051093baff4
Dmitriy <dima@webdima31.ru>
2021-11-25 14:53:48 +0300

    Изучили миграции

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home', ['res' => 5, 'name' => 'Dimon']);
+//        $query = DB::insert("INSERT INTO posts (title, content) VALUES (?, ?)", ['Статья 5', 'Контент статьи 5']);
+//        var_dump($query);
+//        DB::update("UPDATE posts SET created_at = ?, updated_at = ? WHERE created_at IS NULL OR updated_at IS NULL", [NOW(), NOW()]);
+//        DB::delete("DELETE FROM posts WHERE id = :id", ['id' => 4]);
+        DB::beginTransaction();
+        try {
+            DB::update("UPDATE posts SET created_at = ? WHERE created_at IS NULL", [NOW()]);
+            DB::update("UPDATE posts SET updated_at = ? WHERE updated_at IS NULL", [NOW()]);
+            DB::commit();
+        }catch ( \Exception $e) {
+            DB::rollBack();
+            echo $e->getMessage();
+        }
+
+        $posts = DB::select("SELECT * FROM posts WHERE id > :id", ['id' => 0]);
+        return $posts;
+
+        return view('home');
     }
 
     public function test()
